eeafb22d736672be586cd221b731aed32c735ef4
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2019-08-10 01:46:45 -0500

    build 1

---
+++ build/precache-manifest.c977f0a1044a6df0b6150e3a53e6975b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ff8179371df61a8daf085e687a3442",
+    "url": "/index.html"
+  },
+  {
+    "revision": "966e01bf51b8bb553ffb",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f14dc075a34a756a37d2",
+    "url": "/static/css/main.6543074e.chunk.css"
+  },
+  {
+    "revision": "966e01bf51b8bb553ffb",
+    "url": "/static/js/2.106fea24.chunk.js"
+  },
+  {
+    "revision": "f14dc075a34a756a37d2",
+    "url": "/static/js/main.8826f591.chunk.js"
+  },
+  {
+    "revision": "ae74b97544f7ba29d4a0",
+    "url": "/static/js/runtime~main.0aa710bb.js"
+  },
+  {
+    "revision": "de82c64b0013c1e2a22f5a8fb26f04f4",
+    "url": "/static/media/logo.de82c64b.png"
+  }
+]);
